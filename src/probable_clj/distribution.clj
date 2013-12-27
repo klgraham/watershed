3c1abe6edb2815887d4d54768ebc87ed1e5b5193
Kenneth Graham <klgraham@gmail.com>
2013-12-27 16:25:33 -0600

    Added distribution for pair of n-sided dice and some simple examples for computing probabilities

--- src/probable_clj/distribution.clj
+++ src/probable_clj/distribution.clj
@@ -37,132 +37,6 @@
         count
         (/ (.doubleValue n)))))
 
-;; Distribution with random variables uniformly distributed on [0,1].
-(s/defrecord UniformDistribution
-  [r :- Random]
-  Distribution
-  (sample [this] (.nextDouble r))
-  (sample [this n] (into [] (repeatedly n #(.sample this))))
-  (sample-given [this predicate?]
-                (let [a (.sample this)]
-                  (if (predicate? a) a (sample-given this predicate?))))
-  (given [this predicate? n]
-         (into [] (repeatedly n #(.sample-given this predicate?))))
-  (given [this predicate?] (given this predicate? 10000)))
-
-(defn uniform
-  "Factory function to create a UniformDistribution"
-  [] (UniformDistribution. (new Random)))
-
-;(def u (Uniform. (new Random)))
-;(def u (uniform))
-;(def uu (.sample u 5))
-;(println uu)
-;(def u-pos (-> u (.given #(< % 0.2) 5)))
-;(def u-pos (.given u #(< % 0.2) 5))
-;(def uu1 (map (fn [x] (inc x)) uu))
-;(println u-pos)
-
-;; Distribution with random variables normally distributed on (-\inf, \inf).
-;; Has mean 0 and variance 1.
-(s/defrecord StandardNormalDistribution
-             [r :- Random]
-  Distribution
-  (sample [this] (.nextGaussian r))
-  (sample [this n] (into [] (repeatedly n #(.sample this))))
-  (sample-given [this predicate?]
-                (let [a (.sample this)]
-                  (if (predicate? a) a (sample-given this predicate?))))
-  (given [this predicate? n]
-         (into [] (repeatedly n #(.sample-given this predicate?))))
-  (given [this predicate?] (given this predicate? 10000)))
-
-(s/defn normal
-  "Factory function to create a StandardNormalDistribution"
-  [] (StandardNormalDistribution. (new Random)))
-
-;; Distribution with random true/false variables, with probability p of being true.
-(s/defrecord TrueFalseDistribution
-  [r :- Random
-   p :- Double]
-  Distribution
-  (sample [this] (< (.nextDouble r) p))
-  (sample [this n] (into [] (repeatedly n #(.sample this))))
-  (sample-given [this predicate?]
-                (let [a (.sample this)]
-                  (if (predicate? a) a (sample-given this predicate?))))
-  (given [this predicate? n]
-         (into [] (repeatedly n #(.sample-given this predicate?))))
-  (given [this predicate?] (given this predicate? 10000)))
-
-(s/defn true-false
-  "Factory function to create a TrueFalseDistribution"
-  [p :- Double] (TrueFalseDistribution. (new Random) p))
-
-;(def tf (true-false 0.2))
-;(println (.sample tf 5))
-
-;; Distribution with random 1/0 variables, with probability p of being 1.
-;; 1 == hit/win
-;; 0 == miss/loss
-;; p == probability of a hit/win
-(s/defrecord BernoulliDistribution
-  [r :- Random
-   p :- Double]
-  Distribution
-  (sample [this] (let [b (< (.nextDouble r) p)]
-                   (if b 1 0)))
-  (sample [this n] (into [] (repeatedly n #(.sample this))))
-  (sample-given [this predicate?]
-                (let [a (.sample this)]
-                  (if (predicate? a) a (sample-given this predicate?))))
-  (given [this predicate? n]
-         (into [] (repeatedly n #(.sample-given this predicate?))))
-  (given [this predicate?] (given this predicate? 10000)))
-
-(s/defn bernoulli
-  "Factory function to create a BernoulliDistribution"
-  [p :- Double] (BernoulliDistribution. (new Random) p))
-
-;(def bern (bernoulli 0.5))
-;(println (.sample bern 5))
-
-(s/defrecord ExponentialDistribution
-  [r :- Random
-   p :- Double]
-  Distribution
-  (sample [this] (* (/ -1 p) (Math/log (.nextDouble r))))
-  (sample [this n] (into [] (repeatedly n #(.sample this))))
-  (sample-given [this predicate?]
-                (let [a (.sample this)]
-                  (if (predicate? a) a (sample-given this predicate?))))
-  (given [this predicate? n]
-         (into [] (repeatedly n #(.sample-given this predicate?))))
-  (given [this predicate?] (given this predicate? 10000)))
-
-(s/defn exponential
-  "Factory function to create a ExponentialDistribution"
-  [p :- Double] (ExponentialDistribution. (new Random) p))
-
-;; Distribution with random inte uniformly distributed on [low, high].
-(s/defrecord DiscreteUniformDistribution
-  [low :- s/Int
-   high :- s/Int]
-  Distribution
-  (sample [this] (rand-nth (range low (inc high))))
-  (sample [this n] (into [] (repeatedly n #(.sample this))))
-  (sample-given [this predicate?]
-                (let [a (.sample this)]
-                  (if (predicate? a) a (sample-given this predicate?))))
-  (given [this predicate? n]
-         (into [] (repeatedly n #(.sample-given this predicate?))))
-  (given [this predicate?] (given this predicate? 10000)))
-
-(s/defn discrete-uniform
-  "Factory function to create a DiscreteUniformDistribution"
-  [low :- s/Int
-   high :- s/Int] (DiscreteUniformDistribution. low high))
-
 ;; Useful predicates
 (s/defn gt? "Is x greater than y?"
   [y :- s/Number]
@@ -188,3 +62,154 @@
                (<= x high))))
 
 (s/defn eq? [y :- s/Number] (fn [x] (= x y)))
+
+;; Distribution with random variables uniformly distributed on [0,1].
+(s/defrecord UniformDistribution
+  [r :- Random]
+  Distribution
+  (sample [this] (.nextDouble r))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(defn uniform
+  "Factory function to create a UniformDistribution"
+  [] (UniformDistribution. (new Random)))
+
+;(def u (Uniform. (new Random)))
+;(def u (uniform))
+;(def uu (.sample u 5))
+;(println uu)
+;(def u-pos (-> u (.given #(< % 0.2) 5)))
+;(def u-pos (.given u #(< % 0.2) 5))
+;(def uu1 (map (fn [x] (inc x)) uu))
+;(println u-pos)
+
+;; Distribution with random variables normally distributed on (-\inf, \inf).
+;; Has mean 0 and variance 1.
+(s/defrecord StandardNormalDistribution
+             [r :- Random]
+  Distribution
+  (sample [this] (.nextGaussian r))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(s/defn normal
+  "Factory function to create a StandardNormalDistribution"
+  [] (StandardNormalDistribution. (new Random)))
+
+;; Distribution with random true/false variables, with probability p of being true.
+(s/defrecord TrueFalseDistribution
+  [r :- Random
+   p :- Double]
+  Distribution
+  (sample [this] (< (.nextDouble r) p))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(s/defn true-false
+  "Factory function to create a TrueFalseDistribution"
+  [p :- Double] (TrueFalseDistribution. (new Random) p))
+
+;(def tf (true-false 0.2))
+;(println (.sample tf 5))
+
+;; Distribution with random 1/0 variables, with probability p of being 1.
+;; 1 == hit/win
+;; 0 == miss/loss
+;; p == probability of a hit/win
+(s/defrecord BernoulliDistribution
+  [r :- Random
+   p :- Double]
+  Distribution
+  (sample [this] (let [b (< (.nextDouble r) p)]
+                   (if b 1 0)))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(s/defn bernoulli
+  "Factory function to create a BernoulliDistribution"
+  [p :- Double] (BernoulliDistribution. (new Random) p))
+
+;(def bern (bernoulli 0.5))
+;(println (.sample bern 5))
+
+(s/defrecord ExponentialDistribution
+  [r :- Random
+   p :- Double]
+  Distribution
+  (sample [this] (* (/ -1 p) (Math/log (.nextDouble r))))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(s/defn exponential
+  "Factory function to create a ExponentialDistribution"
+  [p :- Double] (ExponentialDistribution. (new Random) p))
+
+;; Distribution with random inte uniformly distributed on [low, high].
+(s/defrecord DiscreteUniformDistribution
+  [low :- s/Int
+   high :- s/Int]
+  Distribution
+  (sample [this] (rand-nth (range low (inc high))))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(s/defn discrete-uniform
+  "Factory function to create a DiscreteUniformDistribution"
+  [low :- s/Int
+   high :- s/Int] (DiscreteUniformDistribution. low high))
+
+;"A pair of n-sided dice. Each die is identical and has sides
+;consecutively numbered from low to high"
+(s/defrecord PairOfNSidedDice
+  [low :- s/Int
+   high :- s/Int]
+  Distribution
+  ;"Given a roll [a b], returns a + b"
+  (sample [this]
+    (let [r (range low (inc high))
+          one (rand-nth r)
+          two (rand-nth r)]
+      (+ one two)))
+  (sample [this n] (into [] (repeatedly n #(.sample this))))
+  (sample-given [this predicate?]
+                (let [a (.sample this)]
+                  (if (predicate? a) a (.sample-given this predicate?))))
+  (given [this predicate? n]
+         (into [] (repeatedly n #(.sample-given this predicate?))))
+  (given [this predicate?] (.given this predicate? 10000)))
+
+(s/defn dice
+  "Factory function to create a DiscreteUniformDistribution"
+  [low :- s/Int
+   high :- s/Int] (PairOfNSidedDice. low high))
--- src/probable_clj/examples.clj
+++ src/probable_clj/examples.clj
@@ -1,5 +1,5 @@
 (ns probable-clj.examples
-  (:require [probable-clj.distribution :as d]))
+  (:use [probable-clj.distribution]))
 
 ;;;;; Contains examples of how to use probable-clj for:
 ;;;;;  * general probability distribution calculations
@@ -8,17 +8,33 @@
 ;;;; These examples use the code that doesn't just use functions,
 ;;;; but also uses protocols and factor functions
 
-(def norm (d/normal))
-(println "Sampling the Standard Normal distribution between +/-1:\n" (-> norm (.given (d/between? -1 1) 5)))
-(println "Prob. of values between +/- 1: " (d/prob norm (d/between? -1 1)))
-(println "Prob. of values between +/- 2: " (d/prob norm (d/between? -2 2)))
-(println "Prob. of values between +/- 3: " (d/prob norm (d/between? -3 3)))
-(println "Prob. of values between +/- 4: " (d/prob norm (d/between? -4 4)))
-(println "Prob. of values between +/- 5: " (d/prob norm (d/between? -5 5)))
+(def norm (normal))
+(println "Sampling the Standard Normal distribution between +/-1:\n" (-> norm (.given (between? -1 1) 5)))
+(println "Prob. of values between +/- 1: " (prob norm (between? -1 1)))
+(println "Prob. of values between +/- 2: " (prob norm (between? -2 2)))
+(println "Prob. of values between +/- 3: " (prob norm (between? -3 3)))
+(println "Prob. of values between +/- 4: " (prob norm (between? -4 4)))
+(println "Prob. of values between +/- 5: " (prob norm (between? -5 5)))
 
 (println "\nIf win prob. is 20%, what is the prob of a win in 10000 Bernoulli trials?")
-(println (d/prob (d/bernoulli 0.2) (d/eq? 1)))
+(println (prob (bernoulli 0.2) (eq? 1)))
 (println "\nWhat about in 1000 Bernoulli trials?")
-(println (d/prob (d/bernoulli 0.2) (d/eq? 1) 1000))
+(println (prob (bernoulli 0.2) (eq? 1) 1000))
 
 ;; Rolling the dice: Composing distributions to obtain others
+;; 6-sided die
+(def die (discrete-uniform 1 6))
+(println "\nProb. of getting a 1, 2, or 3: " (prob die (between? 1 3)))
+
+;; This distribution is useful enough to build it in
+;; pair of 6-sided dice
+(def pair (dice 1 6))
+;(println "\nThere are three ways of rolling a 4: [2, 2], [1, 3], and [3, 1]")
+(println "Prob of rolling a 2: " (prob pair (eq? 2)))
+(println "Prob of rolling a 4: " (prob pair (eq? 4)))
+(println "Prob of rolling a 5: " (prob pair (eq? 5)))
+(println "Prob of rolling a 6: " (prob pair (eq? 6)))
+(println "Prob of rolling a 7: " (prob pair (eq? 7)))
+(println "Prob of rolling a 10: " (prob pair (eq? 10)))
+(println "Prob of rolling a 12: " (prob pair (eq? 12)))
+(println "Prob of rolling a 1-12: " (prob pair (between? 1 12)))
