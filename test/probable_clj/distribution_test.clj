0dde6a1f6c07af875723c6bf3dd3d1e4511ab4b3
Kenneth Graham <klgraham@gmail.com>
2013-12-28 23:35:46 -0500

    Add tests

---
+++ test/probable_clj/distribution_test.clj
@@ -0,0 +1,29 @@
+(ns probable-clj.distribution_test
+  (:use [midje.sweet]
+        [probable-clj.distribution]))
+
+(facts "Probability of rolling a 6-sided die and getting a 1-6 is one."
+       (def die (discrete-uniform 1 6))
+       (prob die (between? 1 6)) => 1.0)
+
+(facts "Probability of rolling a pair of 6-sided dice and getting a 1-12 is one."
+       ;(def die (dice 1 12))
+       (prob (dice 1 6) (between? 1 12)) => 1.0)
+
+(facts "Probability of getting heads when flipping a fair coin is about 1/2."
+       (-> (prob (coin) (eq? 'H))
+           (- 0.5)
+           (Math/abs)
+           (< 0.01)) => true)
+
+(facts "Probability of getting heads when flipping an unfair coin with p=0.3 is 0.3."
+       (-> (prob (biased-coin 0.3) (eq? 'H))
+           (- 0.3)
+           (Math/abs)
+           (< 0.01)) => true)
+
+(facts "Prob of std. normal values within 5 std. deviations is ~99.9%."
+       (-> (prob (normal) (between? -5 5))
+           (- 1.0)
+           (Math/abs)
+           (< 0.001)) => true)
