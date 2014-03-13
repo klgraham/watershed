bd93f7cf5be4ba39aa9cb3374d098311fc402f1d
Kenneth Graham <klgraham@gmail.com>
2014-03-13 13:01:57 -0500

    added lein-release

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
 (defproject watershed "0.1.0-SNAPSHOT"
   :description "Library for working with probabilistic graphical models in Clojure."
   :url "https://github.com/klgraham/watershed"
+
+  :lein-release {:deploy-via :clojars}
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [prismatic/schema "0.1.9"]
                  [org.clojure/core.match "0.2.0"]
                  [com.taoensso/timbre "3.0.0-RC4"]]
+
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}
              :test-libs {:dependencies [[midje "1.6.3"]]}
              :1.4       [:test-libs {:dependencies [[org.clojure/clojure "1.4.0"]]}]
